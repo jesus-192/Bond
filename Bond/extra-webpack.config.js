a92f670335873feb51d932c81c73e543fb91835a
Jesús Vázquez <jvazquez@be-it.com.mx>
2022-08-08 14:54:50 -0500

    commit google sheet

---
+++ Bond/extra-webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+resolve: {
+extensions: ['.js'],
+alias: {
+fs: path.resolve(__dirname, 'src/mocks/fs.mock.js'),
+child_process: path.resolve(
+__dirname,
+'src/mocks/child_process.mock.js'
+),
+'https-proxy-agent': path.resolve(
+__dirname,
+'src/mocks/https-proxy-agent.mock.js',
+),
+},
+},
+};
--- Bond/src/app/app.component.ts
+++ Bond/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bond';
+  contactForm: FormGroup;
+  GoogleSpreadsheet = require('google-spreadsheet');
+  creds = require('../../assets/sheet-api/credentials.json');
+
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, ) {
+    this.contactForm = new FormGroup({
+    fullName: new FormControl(),
+    email: new FormControl(),
+    message: new FormControl()
+    });
+    }
+
+  
+
+  onSubmit() {
+    const jsonObject = this.contactForm.value;
+    console.log('Your form data : ', this.contactForm.value);
+    const doc = new this.GoogleSpreadsheet('1KkdxPOdxxc4qi05LxDdSBR-foHo3q7KDr68oGhUVHpk');
+    doc.useServiceAccountAuth(this.creds, function(err:any) {
+    doc.addRow(1, jsonObject, function (err:any) {
+    if (err) {
+    console.log(err);
+    }
+    });
+    });
+    }
+
 }
